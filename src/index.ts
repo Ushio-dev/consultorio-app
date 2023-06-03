8998d560794a08d66b80d7a6c0409b0a80b7bb0c
Franco <fran14korn@live.com>
2023-06-03 00:38:47 -0300

    cambios

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
 import "reflect-metadata"
+import app from './app'
+
+async function main() {
+    
+    console.log('conectado')
+    app.listen(3000, () => {
+        console.log(`Server on Port ${3000}`)
+    })
+}
+
+main()
