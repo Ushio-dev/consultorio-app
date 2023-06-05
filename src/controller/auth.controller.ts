ee9de6e61e1e99ad50df277901fec2c05a579b7e
Franco <fran14korn@live.com>
2023-06-05 17:15:42 -0300

    se agrego catch de error

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -2,18 +2,21 @@ import { Request, Response } from "express";
 import {loginService, registerService} from '../service/auth.service'
 
 export const register = async (req: Request, res: Response) => {
-    const user = await registerService(req)
+    try {
+        const user = await registerService(req)
 
-    res.json(user).status(200)
+        res.json(user).status(200)
+    } catch (e: any) {
+        res.status(500).json({'msg': 'Invalid Fields'})
+    }
+    
 }
 
 export const login = async (req: Request, res: Response) => {
     try {
         const token = await loginService(req)
-        res.header('auth-token', token).status(200).json('ok')
+        res.header('auth-token', token).status(200).json('Login Success')
     } catch (e: any) {
         res.status(403).json({'msg': 'Invalid email or password'})
-    }
-    
-    
+    }  
 }
